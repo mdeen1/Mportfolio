c02071a57e3a9bf29ea56332e8a0e1cac5799a0f
mdeen1 <34013848+mdeen1@users.noreply.github.com>
2019-05-22 20:00:31 -0700

    Starting on the about me section

--- Portfolio/js/script.js
+++ Portfolio/js/script.js
@@ -5,4 +5,12 @@ $(document).ready(function() {
 		play: 5000,
 		pagination: false
 	});
-})
+
+	var typed = new Typed(".typed", {
+		strings: ["Web Developer.", "Software Engineer.", "Student."],
+		typeSpeed: 70,
+		loop: true,
+		startDelay: 1000,
+		showCursor: false
+	});
+});
