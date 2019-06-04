acd61a05ef73371d0782c1866d22688ea62cdbca
mdeen1 <34013848+mdeen1@users.noreply.github.com>
2019-06-04 11:22:09 -0700

    portfolio section

--- Portfolio/js/script.js
+++ Portfolio/js/script.js
@@ -56,6 +56,47 @@ $(document).ready(function() {
 		}
 	});
 
+	$(".counter").each(function() {
+		var element = $(this);
+		var endVal = parseInt(element.text());
+
+		element.countup(endVal);
+	})
+
+
+
+
+	$("[data-fancybox]").fancybox();
+
+	$(".items").isotope({
+		filter: '*',
+		animationOptions: {
+			duration: 1500,
+			easing: 'linear',
+			queue: false
+		}
+	});
+
+	$("#filters a").click(function() {
+
+		$("#filters .current").removeClass("current");
+		$(this).addClass("current");
+
+		var selector = $(this).attr("data-filter");
+
+		$(".items").isotope({
+			filter: selector,
+			animationOptions: {
+				duration: 1500,
+				easing: 'linear',
+				queue: false
+			}
+		});
+
+		return false;
+
+	});
+
 });
 
 
